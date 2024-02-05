35a3e1e4a5de41bba1cf5d143784f0111aa7e833
olivier-durand <olivier.durand@laplateforme.io>
2024-02-05 10:38:23 +0100

    Projet Python jour01

---
+++ jour01/job21.sql
@@ -0,0 +1,7 @@
+mysql> SELECT COUNT(*) AS nombre_etudiants_18_25 FROM etudiant WHERE age BETWEEN 18 AND 25;
++------------------------+
+| nombre_etudiants_18_25 |
++------------------------+
+|                      3 |
++------------------------+
+1 row in set (0.00 sec)
